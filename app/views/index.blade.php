65952b586a5e4a18e890293e8804e62832d30ccc
Martin Kiforenko Jensby <martin@rootuser.dk>
2014-05-10 22:33:22 +0200

    Clean up

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -4,17 +4,15 @@
 
 	<h1>Nyheder</h1>
 	@foreach($stories as $story)
-		<div class="table">
-			<table class="table table-bordered">
-				<tr>
-					<th colspan="2">{{$story->headline}}</th>
-				</tr>
-				<tr>
-					<td>Dato: {{$story->created_at}} @if($story->updated_at != $story->created_at)<br/>Updateret: {{$story->updated_at}}@endif</td>
-					<td>{{$story->content}}</td>
-				</tr>
-			<table>
-		</div>
+		<table class="table table-bordered">
+			<tr>
+				<th colspan="2">{{$story->headline}}</th>
+			</tr>
+			<tr>
+				<td>Dato: {{$story->created_at}} @if($story->updated_at != $story->created_at)<br/>Updateret: {{$story->updated_at}}@endif</td>
+				<td>{{$story->content}}</td>
+			</tr>
+		<table>
 	@endforeach
 
 @stop
