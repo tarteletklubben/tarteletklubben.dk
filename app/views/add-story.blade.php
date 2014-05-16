2aae275b8c0630e70495d9d255f0cb34ca76c6e8
Martin Kiforenko Jensby <martin@rootuser.dk>
2014-05-16 21:17:00 +0200

    Beautify the add story form a little

--- app/views/add-story.blade.php
+++ app/views/add-story.blade.php
@@ -1,23 +1,28 @@
 @extends('layouts.menu')
 
 @section('content')
-	@if(isset($story))
-		{{Form::model($story, array('action' => array('StoryController@putAddStory', $story->id), 'method' => 'put', 'role' => 'form'))}}
-	@else
-		{{Form::open(array('action' => array('StoryController@postAddStory'), 'role' => 'form'))}}
-	@endif
+	<div class="row">
+		@if(isset($story))
+			{{Form::model($story, array('action' => array('StoryController@putAddStory', $story->id), 'method' => 'put', 'role' => 'form'))}}
+		@else
+			{{Form::open(array('action' => array('StoryController@postAddStory'), 'role' => 'form'))}}
+		@endif
 
-	<div class="form-group">
-		{{Form::label('headline', 'Headline', array('for' => 'headline'))}}
-		{{Form::text('headline', $value = null, array('class' => 'form-control', 'id' => 'headline'))}}
+		<div class="form-group">
+			{{Form::label('headline', 'Overskrift', array('for' => 'headline'))}}
+			{{Form::text('headline', $value = null, array('class' => 'form-control', 'id' => 'headline'))}}
+		</div>
+
+		<div class="form-group">
+			{{Form::label('content', 'Indhold', array('for' => 'content'))}}
+			{{Form::textarea('content', $value = null, array('class' => 'form-control', 'id' => 'content'))}}
+		</div>
 	</div>
-
-	<div class="form-group">
-		{{Form::label('content', 'Content', array('for' => 'content'))}}
-		{{Form::textarea('content', $value = null, array('class' => 'form-control', 'id' => 'content'))}}
+	<div class="row">
+		<div class="col-xs-1 col-xs-offset-11">
+			{{Form::submit(isset($story) ? 'Opdater' : 'Tilføj', $attributes = array('class' => 'btn btn-success'));}}
+		</div>
 	</div>
-
-	{{Form::submit(isset($story) ? 'Opdater' : 'Tilføj');}}
 
 	{{Form::close()}}
 
